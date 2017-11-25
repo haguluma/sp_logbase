80e80da204636d87802db76a3d57cbe1af8710bf
sunny <haguluma0@yahoo.co.jp>
2017-11-25 11:24:14 +0900

    しゅうせい

--- aggregate/clget.sh
+++ aggregate/clget.sh
@@ -1,1 +1,3 @@
-scp worker00:~/utility/Cglog/* ~/sp_logbase/Cglog/
+scp -r sparkmaster:~/utility/Cglog/* ~/sp_logbase/Cglog/
+scp -r general@worker01:~/utility/Cglog/* ~/sp_logbase/Cglog/
+scp -r general@worker00:~/utility/Cglog/* ~/sp_logbase/Cglog/
